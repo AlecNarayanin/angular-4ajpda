6e936cf4c84b3b3104d4e7bba0187e603cf4c8bf
AlecNarayanin <narayanin.alec@hotmail.com>
2021-08-18 18:52:16 +0200

    search fix ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent }
-    ])
+    ]),
+    CommonModule
   ],
   declarations: [
     AppComponent,
